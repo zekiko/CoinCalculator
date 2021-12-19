a5d7039f368d4f06f70afe4454bcaa1b23b7e15d
Çağrı Zeki <cagrizeki@gmail.com>
2021-12-19 19:18:57 +0300

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Coin Calculator Rest API
+
+This is a Spring Boot project for link conversion between web and mobile requests.
+
+### REQUIREMENTS
+* Java 8
+* Maven
+* PostgreSQL
+* Git
+* Docker Client
+
+## RUN THE PROJECT
+
+- Clone the project
+- Configure PostgreSQL password in application.properties
+- Build the project
---
+++ src/main/java/com/calculator/coin/CoinCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinCalculatorApplication.class, args);
+	}
+
+}
