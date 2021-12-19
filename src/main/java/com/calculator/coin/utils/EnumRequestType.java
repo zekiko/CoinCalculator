50b4096592d53c82d0e5296ee1e1c2994ef43734
Çağrı Zeki <cagrizeki@gmail.com>
2021-12-19 19:44:41 +0300

    EnumRequestType.java class implemented.

---
+++ src/main/java/com/calculator/coin/utils/EnumRequestType.java
@@ -0,0 +1,24 @@
+package com.calculator.coin.utils;
+
+public enum EnumRequestType {
+    BTC_USD(Constants.BTC_USD_ENDPOINT, "BTC-USD"),
+    BTC_EUR(Constants.BTC_EUR_ENDPOINT, "BTC-EUR"),
+    ETH_USD(Constants.ETH_USD_ENDPOINT, "ETH-USD"),
+    ETH_EUR(Constants.ETH_EUR_ENDPOINT, "ETH-EUR");
+
+    private final String url;
+    private final String nameStr;
+
+    EnumRequestType(String url, String nameStr) {
+        this.url = url;
+        this.nameStr = nameStr;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getNameStr() {
+        return nameStr;
+    }
+}
