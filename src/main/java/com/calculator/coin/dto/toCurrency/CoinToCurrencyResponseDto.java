0782fad43c71b8e02a8352bb9d1f21849f5355c8
Çağrı Zeki <cagrizeki@gmail.com>
2021-12-19 19:31:56 +0300

    CoinToCurrencyResponseDto class created.

---
+++ src/main/java/com/calculator/coin/dto/toCurrency/CoinToCurrencyResponseDto.java
@@ -0,0 +1,13 @@
+package com.calculator.coin.dto.toCurrency;
+
+public class CoinToCurrencyResponseDto {
+    private float currencyAmount;
+
+    public float getCurrencyAmount() {
+        return currencyAmount;
+    }
+
+    public void setCurrencyAmount(float currencyAmount) {
+        this.currencyAmount = currencyAmount;
+    }
+}
